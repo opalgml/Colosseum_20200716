c2699df230a3b93b370f9cdbc914b21aa5d44cf8
김미희 <opalgml@naver.com>
2020-07-16 22:21:33 +0900

    0716 bak

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/SighUpActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.colosseum_20200716
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SighUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sigh_up)
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}
