55fc1db05b9b3f6e9664aa593e861b9141647912
김미희 <opalgml@naver.com>
2020-07-20 22:19:54 +0900

    주제 목록을 받아서 로그로 출력

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -2,8 +2,14 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.colosseum_20200716.datas.Topic
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<Topic>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +23,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        getTopicListFromServer()
+
+    }
+
+    fun getTopicListFromServer()
+    {
+
+        ServerUtil.getRequestMainInfo(mContext, object: ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
 
     }
 }
