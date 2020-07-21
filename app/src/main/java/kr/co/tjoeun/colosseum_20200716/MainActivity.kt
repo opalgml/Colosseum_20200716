0747b20400dac4518574cadbd53998de6b9e6860
김미희 <opalgml@naver.com>
2020-07-21 19:48:52 +0900

    Topic 클래스에 Json 변환 기능 추가 => 메인 화면에서 JSONArray 파싱할때 사용

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -34,6 +34,27 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestMainInfo(mContext, object: ServerUtil.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
 
+                val data = json.getJSONObject("data")
+
+//                topics 는 [] 임. => JSonArray로 추출해야함
+                val topics = data.getJSONArray("topics")
+
+//                topics 내부에는 JSONObject가 여러개 반복해서 존재
+//                JSON을 들고 있는 배열 => JSONArray
+
+//                for문 이용, topics 내부의 데이터를 하나씩 추출
+                for(i in 0 until topics.length()) //for(i in 0..topics.length-1) 동일함.
+                {
+//                    topics 내부의 데이터를 JSONObject로 추출
+                    val topicObj = topics.getJSONObject(i)
+
+//                    topicObj => Topic 형태의 객체로 변환
+                    val topic = Topic.getTopicFromJson(topicObj)
+
+//                    변환된 객체를 목록에 추가
+                    mTopicList.add(topic)
+                }
+
             }
 
         })
--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Topic.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Topic.kt
@@ -1,9 +1,27 @@
 package kr.co.tjoeun.colosseum_20200716.datas
 
+import org.json.JSONObject
+
 class Topic {
 
     var id = 0
     var title = ""
     var imgageUrl = ""
+
+//    json한 덩어리를 넣으면 => Topic 객체로 변환해주는 기능
+    companion object
+    {
+        fun getTopicFromJson(json:JSONObject) : Topic
+        {
+//            변환시켜줄 Topic 객체 생성
+            val topic = Topic()
+//            만든 객체의 내용물들을 json을 이용해서 채움
+            topic.id = json.getInt("id")
+            topic.title = json.getString("title")
+            topic.imgageUrl = json.getString("img_url")
+//            완성된 객체 리턴
+            return topic
+        }
+    }
 
 }
