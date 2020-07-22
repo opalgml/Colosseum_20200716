30b80d6c9a6ae584767e0b18aea67a6bb37aeaf7
김미희 <opalgml@naver.com>
2020-07-22 21:19:24 +0900

    의견 목록을 뿌려주는 Adapter 작업 -> ReplyAdapter만 작업

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -5,33 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
-import com.bumptech.glide.Glide
 import kr.co.tjoeun.colosseum_20200716.R
 import kr.co.tjoeun.colosseum_20200716.datas.Reply
-import kr.co.tjoeun.colosseum_20200716.datas.Topic
 
-class ReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
+class ReplyAdapter(val mContext: Context, resId : Int, val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var tempRow = convertView
+        var tempRow = convertView //convertView 는 값을 변경할 수 없기 때문에 tempRow를 이용하여 값을 넣음
         if(tempRow == null)
         {
-            tempRow = inf.inflate(R.layout.activity_reply_list, null)
+            tempRow = inf.inflate(R.layout.reply_list_item, null)
         }
 
         val row = tempRow!!
-
-//        val topicImg = row.findViewById<ImageView>(R.id.topicImg)
-//        val topicTitleTxt = row.findViewById<TextView>(R.id.topicTitleTxt)
-//
-//        val data = mList[position]
-//
-//        topicTitleTxt.text = data.title
-//        Glide.with(mContext).load(data.imageUrl).into(topicImg)
 
         return row
     }
