df1c982051793576685a4927127d1589ffc80cfb
김미희 <opalgml@naver.com>
2020-07-23 20:58:51 +0900

    서버와 휴대폰 설정시간대간의 시차를 보정

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Reply.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Reply.kt
@@ -36,6 +36,17 @@ class Reply {
 
 //            멤버변수인 Calendar 변수에게 데이터를 적용
             r.writtenDateTime.time = sdf.parse(createdAtString)
+
+//            핸드폰의 시간대와 서버 시간대 "시차"를 구해서, 작성 일시의 시간값을 조정
+
+//          1. 휴대폰의 시간대가 어디 시간대인지 변수로 저장
+            val myPhoneTimeZone = r.writtenDateTime.timeZone // 한국시간대로 설정
+
+//          2. 서버와 몇시간 차이가 나는지 변수로 저장 => 밀리초까지 계산된 시차 => 시간단위로 변경해줘야함.
+            val timeOffset = myPhoneTimeZone.rawOffset / 1000 / 60 / 60
+
+//          3. 게시글 작성시간을 timeOffset만큼 시간값을 더해준다.
+            r.writtenDateTime.add(Calendar.HOUR, timeOffset)
     
             return r
     
