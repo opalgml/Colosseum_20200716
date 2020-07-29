2f42a408d6563b698a06897f00a14e513b9b8c72
김미희 <opalgml@naver.com>
2020-07-29 20:58:58 +0900

    알림 데이터 + xml 파일 추가 => 리스트뷰 완성해보자

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/NotificationListActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.colosseum_20200716
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_list)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/datas/Notification.kt
@@ -0,0 +1,10 @@
+package kr.co.tjoeun.colosseum_20200716.datas
+
+import java.util.*
+
+class Notification {
+    var id = 0
+    var title = ""
+//    알림 발생 시간 변수
+    val createdAtCal = Calendar.getInstance()
+}
